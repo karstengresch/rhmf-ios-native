ade881824b46fe32b37c642964ee26d5b2820f53
Karsten Gresch <git-2015@by-doing.org>
2016-12-06 23:56:04 +0100

    Added base iOS standalone iPhone app

---
+++ rhmf-ios-native/rhmf-ios-native/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rhmf-ios-native
+//
+//  Created by Karsten Gresch on 06.12.16.
+//  Copyright © 2016 Red Hat MW SAs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
